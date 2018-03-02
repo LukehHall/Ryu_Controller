63cb3565be2fa288869230f2ced272b2c4b3d782
Luke Hall <LukehHall@users.noreply.github.com>
2018-03-02 13:54:00 +0000

    - Added timer - Added DDoS detected flag

--- Ryu_Controller/DDoSDetection.py
+++ Ryu_Controller/DDoSDetection.py
@@ -2,6 +2,7 @@
 # DDoSDetection - DDoS Detection Class
 
 from ryu.lib.packet import packet, ethernet
+from threading import Timer
 
 
 class DDoSDetection:
@@ -9,8 +10,10 @@ class DDoSDetection:
     def __init__(self):
         super(DDoSDetection, self).__init__()
         self.ip_dict = {}           # Dictionary to hold ip addresses and number of times they have been seen
-        self.packet_threshold = 10  # packets/sec threshold
-        self.timer_running = False  # Boolean for timer status
+        self.packet_threshold = 10  # Packets/sec threshold
+        self.ddos_detected = False  # Flag for whether DDoS detected
+        self.timer_running = False  # Flag for timer status
+        self.timer = Timer(10, self.timer_ended())   # 10 second interval, self.timer_ended() is handler
 
     def read_packet(self, pkt):
         """
@@ -18,6 +21,8 @@ class DDoSDetection:
         :param pkt: incoming packet
         :return: None
         """
+        if not self.timer_running:
+            self.start_timer()
         if pkt.isInstance(packet.Packet()):
             # pkt is a packet, can be used
             print("Input is a packet")
@@ -48,6 +53,8 @@ class DDoSDetection:
             if src_ip == ip:
                 # ip already exists, increment value
                 value += 1
+                if value >= self.packet_threshold:  # check number of packets from ip
+                    self.ddos_detected = True       # Set DDoS flag
                 return
         # Out of for loop, IP doesn't exist
         self.add_new_ip(src_ip)
@@ -64,10 +71,27 @@ class DDoSDetection:
         Starts timer & sets status boolean
         :return: None
         """
+        self.timer_running = True
+        self.timer.run()
+
+    def timer_ended(self):
+        """
+        Handler for timer ending, reset dict and timer_running
+        :return:
+        """
+        self.timer_running = False
+        self.reset_ip_dict()
 
     def get_timer_status(self):
         """
-        Getter for whether timer running
+        Getter for whether timer is running
         :return: (Boolean) timer_running
         """
         return self.timer_running
+
+    def cancel_timer(self):
+        """
+        Cancel timer
+        :return:
+        """
+        self.timer.cancel()
